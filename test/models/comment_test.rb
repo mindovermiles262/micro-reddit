eb900b6b491b1ecc5fc29329790a857cbbaea4ff
Erik Johnson <mindovermiles262@gmail.com>
2017-06-14 21:15:14 -0700

    add comments model

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -3,11 +3,13 @@ require 'test_helper'
 class CommentTest < ActiveSupport::TestCase
 
   def setup
-    @comment = Comment.new(body:'This is a comment', user_id: 1, post_id: 1, )
+    User.create(name: 'Dr. Who', email: 'foo@bar.com')
+    Post.create(title: 'Hello, World', body: 'Lorem ipsum dolor sit')
+    @comment = Comment.new(body: 'This is a comment', user_id: User.first.id, post_id: Post.first.id )
   end
 
   test 'body is present' do
-    @comment.body = ' '
+    @comment.body = '  '
     assert_not @comment.valid?
   end
 
